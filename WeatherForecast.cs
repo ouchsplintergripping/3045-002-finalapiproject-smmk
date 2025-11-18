c5e83f0ff0e69f3b5fa6cf978fda137518935aba
ouchsplintergripping <brzezisj@mail.uc.edu>
2025-11-17 19:40:33 -0500

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _3045_002_finalapiproject_smmk
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
